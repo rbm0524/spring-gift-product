b8d4a78d5a369db558f724034c0c116f8f5840d7
rbm0524 <rbm0524@naver.com>
2024-06-27 17:14:57 +0900

    feat: Product 클래스 구현

---
+++ src/main/java/gift/Model/Product.java
@@ -0,0 +1,40 @@
+package gift.Model;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+    private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
