22bea882980e8f6b81a8b041620a44bf6297f628
rbm0524 <rbm0524@naver.com>
2024-06-28 11:46:13 +0900

    fix : updateProduct의 기능을 Service에서 수행하도록 변경, init() 삭제, deleteProduct 수정

--- src/main/java/gift/Service/ProductService.java
+++ src/main/java/gift/Service/ProductService.java
@@ -1,49 +1,44 @@
 package gift.Service;
 
 import gift.Model.Product;
-import jakarta.annotation.PostConstruct;
+import gift.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
+
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
     private final Map<Long, Product> products = new HashMap<>();
     private final AtomicLong idCounter = new AtomicLong(); //thread-safe
 
-    @PostConstruct
-    public void init() {
-        Product product = new Product();
-        product.setName("아이스 카페 아메리카노 T");
-        product.setPrice(4500);
-        product.setImageUrl("https://img1.kakaocdn.net/thumb/C320x320@2x.fwebp.q82/?fname=https%3A%2F%2Fst.kakaocdn.net%2Fproduct%2Fgift%2Fproduct%2F20231010111814_9a667f9eccc943648797925498bdd8a3.jpg");
-        saveProduct(product);
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public List<Product> getAllProducts() {
-        return new ArrayList<>(products.values());
+        return productRepository.findAllProduct();
     }
 
     public Optional<Product> getProductById(Long id) {
-        return Optional.ofNullable(products.get(id));
+        return productRepository.findById(id);
     }
 
     public Product saveProduct(Product product) {
-        if (product.getId() != null && products.containsKey(product.getId())) {
-            return null;
-        }
+        product.setId(idCounter.incrementAndGet());
+        return productRepository.saveProduct(product);
+    }
 
-        if (product.getId() == null) {
-            product.setId(idCounter.incrementAndGet());
-        }
-        products.put(product.getId(), product);
-        return product;
+    public void updateProduct(Long id, Product productDetails) {
+        productRepository.updateProduct(id, productDetails);
     }
 
     public void deleteProduct(Long id) {
-        if (products.containsKey(id)) {
-            idCounter.decrementAndGet();
-            products.remove(id);
-        }
+        productRepository.deleteProductById(id);
+        idCounter.decrementAndGet();
     }
 }
