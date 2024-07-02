f13baeeb29e028da3ccd38b8273f75f7f32f831e
rbm0524 <rbm0524@naver.com>
2024-07-02 14:52:08 +0900

    fix : 생성자, getter, setter 추가

--- src/main/java/gift/Model/Product.java
+++ src/main/java/gift/Model/Product.java
@@ -6,6 +6,54 @@ public class Product {
     private String name;
     private double price;
     private String imageUrl;
+    private boolean isDeleted;
+
+    public Product() {
+
+    }
+
+    public Product(Long id, String name, double price, String imageUrl, boolean isDeleted) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.isDeleted = isDeleted;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public double getPrice() {
+
+        return price;
+    }
+
+    public String getImageUrl() {
+
+        return imageUrl;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
@@ -13,29 +61,5 @@ public class Product {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
     }
 }
